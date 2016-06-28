a65268f574a80d5d703fa2ea8b8ee55f5e498341
albertosouza <alberto.souza.99@gmail.com>
2016-06-28 14:28:15 -0300

    update projects package.json

--- bin/up-projects.js
+++ bin/up-projects.js
@@ -1,22 +1,21 @@
 var ncu = require('npm-check-updates');
 var async = require('async');
 
-
 async.series([
-  function updateApp(done) {
-    ncu.run({
-      packageFile: 'app/templates/_package.json',
-      // Any command-line option can be specified here.
-      // These are set by default:
-      // silent: true,
-      jsonUpgraded: false,
-      upgrade: true,
-      upgradeAll: true
-    }).then(function () {
-      console.log('Upgrated app project');
-      done();
-    }).catch(done);
-  },
+  // function updateApp(done) {
+  //   ncu.run({
+  //     packageFile: 'app/templates/_package.json',
+  //     // Any command-line option can be specified here.
+  //     // These are set by default:
+  //     // silent: true,
+  //     jsonUpgraded: false,
+  //     upgrade: true,
+  //     upgradeAll: true
+  //   }).then(function () {
+  //     console.log('Upgrated app project');
+  //     done();
+  //   }).catch(done);
+  // },
   function updateBlog(done) {
     ncu.run({
       packageFile: 'blog/templates/_package.json',
@@ -64,5 +63,3 @@ async.series([
 ], function doneAll(err) {
   if (err) console.error(err);
 })
-
-
