75a100499f66033b11faa7bec9850b0e4744a085
albertosouza <alberto.souza.99@gmail.com>
2017-03-14 22:45:57 -0300

    update app generator to yeoman v1

--- app/index.js
+++ app/index.js
@@ -1,15 +1,17 @@
-'use strict';
-var _s = require('underscore.string');
-var yeoman = require('yeoman-generator');
-var yosay = require('yosay');
-var path = require('path');
-var questions = require('../questions');
-var _ = require('lodash');
+const _s = require('underscore.string'),
+  Generator = require('yeoman-generator'),
+  yosay = require('yosay'),
+  path = require('path'),
+  questions = require('../questions'),
+  _ = require('lodash');
 
-var WejsGenerator = yeoman.Base.extend({
-  constructor: function () {
-    yeoman.Base.apply(this, arguments);
-    this.sourceRoot(path.resolve(__dirname, '../templates/default'))
+let we;
+
+module.exports = class extends Generator {
+  constructor(args, opts) {
+    super(args, opts);
+
+    this.sourceRoot(path.resolve(__dirname, '../templates/default'));
 
     this.npmModulesToInstall = [
       'async', 'lodash', 'we-core'
@@ -24,7 +26,7 @@ var WejsGenerator = yeoman.Base.extend({
       'we-test-tools'
     ];
 
-    this.wejsPLuginsToInstall = {}
+    this.wejsPLuginsToInstall = {};
 
     this.argument('name', { type: String, required: false });
     this.option('skip-install', {
@@ -45,77 +47,122 @@ var WejsGenerator = yeoman.Base.extend({
     this.option('not-create-first-user', {
       desc: 'Skip user creation'
     });
-  },
-  prompting: function () {
+
+    this.doneAll = function doneAll(err) {
+      if ( err ) we.log.error('Error:', err);
+      we.exit(process.exit);
+    };
+  }
+
+  prompting() {
     this.log(yosay(
       'We.js clean app project generator! |o/ |o/ \n generate one testable we.js project!'
     ));
 
-    var prompts = questions.bind(this)();
+    const prompts = questions.bind(this)();
 
     return this.prompt(prompts)
-    .then(function (props) {
+    .then( (props)=> {
       this.name = (this.name || props.name);
       this.projectName = _s.slugify(this.name);
 
       this.appConfigs = _.merge(this.options, props);
       this.projectFolder = this.projectName + '/';
-    }.bind(this));
-  },
-  writing: {
-    writeFiles: function () {
-      if (this.appConfigs.createFirstUser && this.appConfigs.createFirstUser == 'yes') {
-        this.wejsPLuginsToInstall['we-plugin-user'] = true;
-        this.wejsPLuginsToInstall['we-plugin-auth'] = true;
-      }
+    });
+  }
 
-      this.template('README.md.ejs', this.projectFolder + 'README.md');
-      this.template('_package.json', this.projectFolder + 'package.json');
-
-      this.template('_install.js', this.projectFolder + 'install.js');
-      this.template('_local.js', this.projectFolder + 'config/local.js');
-    },
-    copyfiles: function () {
-      this.directory('config', this.projectFolder + 'config');
-      this.directory('files', this.projectFolder + 'files');
-      this.directory('server', this.projectFolder + 'server');
-      this.directory('test', this.projectFolder + 'test');
-      this.copy('gitignore', this.projectFolder + '.gitignore');
-      this.copy('jshintrc', this.projectFolder +  '.jshintrc');
-
-      this.copy('app.js', this.projectFolder + 'app.js');
-
-      this.copy('plugin.js', this.projectFolder +  'plugin.js');
+  writeFiles() {
+    if (this.appConfigs.createFirstUser && this.appConfigs.createFirstUser == 'yes') {
+      this.wejsPLuginsToInstall['we-plugin-user'] = true;
+      this.wejsPLuginsToInstall['we-plugin-auth'] = true;
     }
-  },
-  install: function() {
+
+    this.fs.copyTpl(
+      this.templatePath('README.md.ejs'),
+      this.destinationPath(this.projectFolder + 'README.md'),
+      this
+    );
+
+    this.fs.copyTpl(
+      this.templatePath('_package.json'),
+      this.destinationPath(this.projectFolder + 'package.json'),
+      this
+    );
+
+    this.fs.copyTpl(
+      this.templatePath('_install.js'),
+      this.destinationPath(this.projectFolder + 'install.js'),
+      this
+    );
+
+    this.fs.copyTpl(
+      this.templatePath('_local.js'),
+      this.destinationPath(this.projectFolder + 'config/local.js'),
+      this
+    );
+  }
+
+  copyfiles() {
+    this.fs.copy(
+      this.templatePath('config'),
+      this.destinationPath(this.projectFolder + 'config')
+    );
+    this.fs.copy(
+      this.templatePath('files'),
+      this.destinationPath(this.projectFolder + 'files')
+    );
+    this.fs.copy(
+      this.templatePath('server'),
+      this.destinationPath(this.projectFolder + 'server')
+    );
+    this.fs.copy(
+      this.templatePath('test'),
+      this.destinationPath(this.projectFolder + 'test')
+    );
+    this.fs.copy(
+      this.templatePath('gitignore'),
+      this.destinationPath(this.projectFolder + '.gitignore')
+    );
+    this.fs.copy(
+      this.templatePath('jshintrc'),
+      this.destinationPath(this.projectFolder + '.jshintrc')
+    );
+    this.fs.copy(
+      this.templatePath('app.js'),
+      this.destinationPath(this.projectFolder + 'app.js')
+    );
+    this.fs.copy(
+      this.templatePath('plugin.js'),
+      this.destinationPath(this.projectFolder + 'plugin.js')
+    );
+  }
+
+  install() {
     if (this.appConfigs.skipInstall) return;
 
     switch (this.appConfigs.dbDialect) {
       case 'postgres':
-        this.npmModulesToInstall.push('pg')
-        this.npmModulesToInstall.push('pg-hstore')
+        this.npmModulesToInstall.push('pg');
+        this.npmModulesToInstall.push('pg-hstore');
         break;
       default:
-        this.npmModulesToInstall.push('mysql')
-        this.npmModulesToInstall.push('express-mysql-session')
+        this.npmModulesToInstall.push('mysql');
+        this.npmModulesToInstall.push('express-mysql-session');
     }
     // enter in folder
     process.chdir(path.resolve(this.projectFolder) );
 
     this.npmModulesToInstall = this.npmModulesToInstall
-      .concat(Object.keys(this.wejsPLuginsToInstall))
+      .concat(Object.keys(this.wejsPLuginsToInstall));
 
     this.log('Installing the dependencies: ' + this.npmModulesToInstall.join(' '));
     // modules
     this.npmInstall(this.npmModulesToInstall, { 'save': true });
     // dev modules
     this.npmInstall(this.devNpmModulesToInstall, { 'saveDev': true });
-  },
+  }
 
-  end: function() {
+  end() {
     this.log('DONE');
   }
-});
-
-module.exports = WejsGenerator;
+};
